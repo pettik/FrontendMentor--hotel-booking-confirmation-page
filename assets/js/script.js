fdd901b11440888c3e0f7739bcf1389299f536d4
pettik <petrbednarski@gmail.com>
2026-09-06 22:44:40 +0200

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,40 @@
-const menuIcon = document.getElementById("menu-main-icon");
-const sidebarPanel = document.getElementById("sidebar");
-const closeIcon = document.getElementById("close-icon");
-const backdrop = document.querySelector(".backdrop");
-let menuIconImg = menuIcon.querySelector("img");
+const menuIcon = document.getElementById('menu-main-icon');
+const sidebarPanel = document.getElementById('sidebar');
+const closeIcon = document.getElementById('close-icon');
+const backdrop = document.querySelector('.backdrop');
+let menuIconImg = menuIcon.querySelector('img');
 
-closeIcon.addEventListener("click", function () {
+closeIcon.addEventListener('click', function () {
   closeSidebar();
 });
 
-menuIcon.addEventListener("click", function () {
-  sidebarPanel.classList.add("sidebar--open");
-  backdrop.classList.add("backdrop--visible");
-  backdrop.setAttribute("aria-hidden", "false");
+menuIcon.addEventListener('click', function () {
+  sidebarPanel.classList.add('sidebar--open');
+  backdrop.classList.add('backdrop--visible');
+  backdrop.setAttribute('aria-hidden', 'false');
 
-  if (menuIcon.getAttribute("aria-expanded") === "true") {
-    menuIcon.setAttribute("aria-expanded", "true");
-    menuIconImg.src = "./assets/images/icon-close.svg";
-    menuIcon.setAttribute("aria-label", "Close menu");
-  }
+  menuIcon.setAttribute('aria-expanded', 'true');
+  menuIconImg.src = './assets/images/icon-close.svg';
+  menuIcon.setAttribute('aria-label', 'Close menu');
 });
 
 // Když uživatel klikne mimo sidebar na tmavý backdrop, zavři sidebar a zároveň skryj backdrop.
-backdrop.addEventListener("click", function () {
+backdrop.addEventListener('click', function () {
   closeSidebar();
 });
 
 // Pomocí ESC zavřít oteveřené mobilní menu
-document.addEventListener("keydown", function (event) {
-  if (event.key === "Escape") {
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
     closeSidebar();
   }
 });
 
 function closeSidebar() {
-  sidebarPanel.classList.remove("sidebar--open");
-  backdrop.classList.remove("backdrop--visible");
-  menuIcon.setAttribute("aria-expanded", "false");
-  menuIcon.setAttribute("aria-label", "Open menu");
-  backdrop.setAttribute("aria-hidden", "true");
+  sidebarPanel.classList.remove('sidebar--open');
+  backdrop.classList.remove('backdrop--visible');
+  menuIcon.setAttribute('aria-expanded', 'false');
+  menuIconImg.src = './assets/images/icon-menu.svg';
+  menuIcon.setAttribute('aria-label', 'Open menu');
+  backdrop.setAttribute('aria-hidden', 'true');
 }
